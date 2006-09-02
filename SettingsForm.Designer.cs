7bf4e50ceda0acd61beba0ad640dd374e317acc9
StevilKnevil <stevilknevil@mail.com>
2006-09-02 18:54:55 +0000

    Tidied up skill planner code

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -535,7 +535,7 @@ namespace EVEMon
             this.label8.Text = "From address:";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // tabControl1
+            // tabControl
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage5);
@@ -543,7 +543,7 @@ namespace EVEMon
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(3, 3);
-            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Name = "tabControl";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(396, 494);
             this.tabControl1.TabIndex = 5;
