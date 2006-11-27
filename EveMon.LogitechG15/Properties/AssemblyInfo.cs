ac729abf5879de9d2e42474ac9df86bafad0b1ad
Anders <anders@mail.com>
2006-11-27 15:59:29 +0000

    Release 1.1.4b

--- EveMon.LogitechG15/Properties/AssemblyInfo.cs
+++ EveMon.LogitechG15/Properties/AssemblyInfo.cs
@@ -1,22 +1,21 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EVEMon.LogitechG15")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EVEMon.LogitechG15")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly : AssemblyTitle("EVEMon.LogitechG15")]
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 296 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EveMon.LogitechG15")] 
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("EVEMon Development Team")]
+[assembly : AssemblyProduct("EVEMon")]
+[assembly: AssemblyCopyright("Copyright © 2006, Timothy Fries")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly : ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e910b4d6-3a95-4484-8084-bee973f2ef89")]
@@ -28,6 +27,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.4.292")]
+[assembly : AssemblyVersion("1.1.4.296")]
