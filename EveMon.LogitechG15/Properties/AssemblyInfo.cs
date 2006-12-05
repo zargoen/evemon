174c6b9a06f86bf082cc43bf3b3bc328f5b5e17c
Anders <anders@mail.com>
2006-12-05 21:34:22 +0000

    I'm nipping the "how come cutting edge and release are the same?" problem in the bud.

--- EveMon.LogitechG15/Properties/AssemblyInfo.cs
+++ EveMon.LogitechG15/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly : AssemblyTitle("EVEMon.LogitechG15")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 372 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EveMon.LogitechG15")] 
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 387 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EveMon.LogitechG15")] 
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("EVEMon Development Team")]
 [assembly : AssemblyProduct("EVEMon")]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly : AssemblyVersion("1.1.6.372")]
+[assembly : AssemblyVersion("1.1.7.387")]
