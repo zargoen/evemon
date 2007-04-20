7fcc3e250b29298553b7fdc5469765eff6030b27
Anders <anders@mail.com>
2007-04-20 14:58:38 +0000

    project wrangling

--- EVEMon.PieChart/Properties/AssemblyInfo.cs
+++ EVEMon.PieChart/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EVEMon.PieChart")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 718 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon.PieChart")] 
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 728 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon.PieChart")] 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.9.718")]
+[assembly: AssemblyVersion("1.1.9.728")]
