68ba139dc4bc0b2cf61e93372f7354403d812a3b
Eewec <eewec@mail.com>
2007-07-07 12:24:17 +0000

    And here's one of the files Brad missed.... now where is constr.gif?

---
+++ Schedule/ScheduleCalendar.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EVEMon.Common.Schedule;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace EVEMon.Schedule
+{
+    public class ScheduleCalendar : CalendarControl
+    {
+        protected Font smallFont = null;
+
+        public Font EntryFont
+        {
+            get { return smallFont; }
+            set { smallFont = value; }
+        }
+
+        protected Color blocking_color = Color.Red;
+        public Color BlockingColor
+        {
+            get { return blocking_color; }
+            set { blocking_color = value; }
+        }
+
+        protected Color single_color = Color.Blue;
+        protected Color single_color2 = Color.LightBlue;
+        public Color SingleColor
+        {
+            get { return single_color; }
+            set { single_color = value; }
+        }
+        public Color SingleColor2
+        {
+            set { single_color2 = value; }
+            get { return single_color2; }
+        }
+
+        protected Color recurring_color = Color.Green;
+        protected Color recurring_color2 = Color.LightGreen;
+
+        public Color RecurringColor
+        {
+            get { return recurring_color; }
+            set { recurring_color = value; }
+        }
+        public Color RecurringColor2
+        {
+            get { return recurring_color2; }
+            set { recurring_color2 = value; }
+        }
+
+        protected List<ScheduleEntry> entries = new List<ScheduleEntry>();
+        public List<ScheduleEntry> Entries
+        {
+            get { return entries; }
+        }
+
+        public ScheduleCalendar() : base()
+        {
+            SetupDefaultResources();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            smallFont.Dispose(); smallFont = null;
+        }
+
+        protected void SetupDefaultResources()
+        {
+            smallFont = new Font(this.Font.FontFamily, 7.0f, FontStyle.Regular);
+        }
+
+        protected override void PaintMonthEntriesForDay(Graphics g, DateTime datetime, Rectangle cellRect)
+        {            
+            List<ScheduleEntry> todays = new List<ScheduleEntry>();
+            foreach (ScheduleEntry entry in entries)
+            {
+                if (entry.IsToday(datetime))
+                {
+                    todays.Add(entry);
+                }
+            }
+
+            // Sort Todays Entries Alphabetically
+            todays.Sort(new ScheduleEntryComparisonTitle());
+
+            Rectangle rect = cellRect;
+            rect.X++;
+            rect.Y += 14;
+            rect.Width -= 1;
+            rect.Height = 11;
+
+
+            List<ScheduleEntry>.Enumerator e = todays.GetEnumerator();
+            if (e.MoveNext())
+            {
+                while (e.Current != null)
+                {
+                    ScheduleEntry entry = e.Current;
+                    int q = entry.Title.Length;
+
+                    if (entry is SimpleScheduleEntry)
+                    {
+                        // Setup a nice Brush
+                        Brush fillBrush = null;
+                        if ((entry.ScheduleEntryOptions & ScheduleEntryOptions.Blocking) != 0)
+                        {
+                            fillBrush = new LinearGradientBrush(new Point(rect.X, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height), blocking_color, single_color2);
+                        }
+                        else
+                        {
+                            fillBrush = new LinearGradientBrush(new Point(rect.X, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height), single_color, single_color2);
+                        }
+
+                        using (fillBrush)
+                        {
+                            // Check if the text fits
+                            Size textsize = TextRenderer.MeasureText(entry.Title, smallFont);
+                            if (textsize.Width <= rect.Width)
+                            {
+                                g.FillRectangle(fillBrush, rect);
+                                TextRenderer.DrawText(g, entry.Title, smallFont, new Point(rect.X + 1, rect.Y), Color.White);
+                            }
+                            else
+                            {
+                                // Make sure the text fits
+                                string shorttext = entry.Title + "..";
+                                for (int i = entry.Title.Length - 1; i > 4; i--)
+                                {
+                                    shorttext = entry.Title.Substring(0, i) + "..";
+                                    textsize = TextRenderer.MeasureText(shorttext, smallFont);
+                                    if (textsize.Width <= rect.Width)
+                                    {
+                                        break;
+                                    }
+                                }
+                                g.FillRectangle(fillBrush, rect);
+                                TextRenderer.DrawText(g, shorttext, smallFont, new Point(rect.X + 1, rect.Y), Color.White);
+                            }
+                        }
+                    }
+                    else if (entry is RecurringScheduleEntry)
+                    {
+                        // Setup a nice Brush
+                        Brush fillBrush = null;
+                        if ((entry.ScheduleEntryOptions & ScheduleEntryOptions.Blocking) != 0)
+                        {
+                            fillBrush = new LinearGradientBrush(new Point(rect.X, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height), blocking_color, recurring_color2);
+                        }
+                        else
+                        {
+                            fillBrush = new LinearGradientBrush(new Point(rect.X, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height), recurring_color, recurring_color2);
+                        }
+
+                        using (fillBrush)
+                        {
+
+                            Size textsize = TextRenderer.MeasureText(entry.Title, smallFont);
+                            if (textsize.Width <= rect.Width)
+                            {
+                                g.FillRectangle(fillBrush, rect);
+                                TextRenderer.DrawText(g, entry.Title, smallFont, new Point(rect.X + 1, rect.Y), Color.White);
+                            }
+                            else
+                            {
+                                // Make sure the text fits
+                                string shorttext = entry.Title + "..";
+                                for (int i = entry.Title.Length - 1; i > 4; i--)
+                                {
+                                    shorttext = entry.Title.Substring(0, i) + "..";
+                                    textsize = TextRenderer.MeasureText(shorttext, smallFont);
+                                    if (textsize.Width <= rect.Width)
+                                    {
+                                        break;
+                                    }
+                                }
+                                g.FillRectangle(fillBrush, rect);
+                                TextRenderer.DrawText(g, shorttext, smallFont, new Point(rect.X + 1, rect.Y), Color.White);
+                            }
+                        }
+                    }
+
+                    rect.Y += rect.Height + 1;
+
+                    // Check if we have room for one more entry?
+                    if (rect.Y + rect.Height > cellRect.Y + cellRect.Height)
+                    {
+                        // No, are there more entries?
+                        if (e.MoveNext())
+                        {
+                            // Yes, Draw something to let the user know
+                            int toomuch = rect.Y + rect.Height - (cellRect.Y + cellRect.Height);
+                            rect.Height -= toomuch;
+
+                            // Make sure LinearGradientBrush doesn't get into any troubles (Out Of Memory Because both points are at the same position)
+                            if (rect.Height == 0) rect.Height = 1;
+
+
+                            using (Brush brush = new LinearGradientBrush(new Point(rect.X, rect.Y), new Point(rect.X, rect.Y + rect.Height), Color.Gray, Color.LightGray))
+                            {
+                                g.FillRectangle(brush, rect);
+                            }                            
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        // Yes, we have more room
+                        e.MoveNext();
+                    }
+                }
+            }
+        }
+    }
+}
