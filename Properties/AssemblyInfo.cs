70c417d2f19acb64b747afe5d543df2fd59d71d6
AraanSunn <araansunn@mail.com>
2008-03-04 14:05:17 +0000

    Assembly files for 1.2.5.1170

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly : AssemblyTitle("EVEMon")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 1005 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk")] 
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 1170 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EveMon/EVEMon/trunk")] 
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("EVEMon Development Team")]
 [assembly : AssemblyProduct("EVEMon")]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly : AssemblyVersion("1.2.4.1005")]
+[assembly : AssemblyVersion("1.2.5.1170")]
