3e62a131aea2867050291936002469afa3d8533a
Richard Slater <richard.slater@hotmail.com>
2009-12-21 20:02:24 +0000

    Removing 1.2.8 code base, to replace with Foundations/1.3.0

--- EVEMon.WinHook/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// EVEMon.WinHook.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
--- lgLcdClassLibrary/ReadMe2.txt
+++
@@ -1,3 +0,0 @@
-This was sourced from a zip file downloaded from http://www.cabhair-cainte.com/ismiselemeas/
-
-I would like to take this opportunity to say thankyou.
