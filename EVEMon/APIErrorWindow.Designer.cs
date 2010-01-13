a37203e7941d5cea3bdda19fc1e258827af31702
Richard Slater <richard.slater@hotmail.com>
2010-01-13 17:42:02 +0000

    Fixes Ticket #1380:  * Prevents the error information from overflowing in the APIErrorWindow.

--- EVEMon/APIErrorWindow.Designer.cs
+++ EVEMon/APIErrorWindow.Designer.cs
@@ -28,74 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.errorLabel = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.borderPanel1 = new EVEMon.Controls.BorderPanel();
+            this.iconPictureBox = new System.Windows.Forms.PictureBox();
+            this.detailsPanel = new EVEMon.Controls.BorderPanel();
             this.detailsTextBox = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.borderPanel1.SuspendLayout();
+            this.errorTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox)).BeginInit();
+            this.detailsPanel.SuspendLayout();
+            this.errorTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.errorLabel);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(13, 13);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(526, 41);
-            this.panel1.TabIndex = 0;
             // 
             // errorLabel
             // 
-            this.errorLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.errorLabel.Location = new System.Drawing.Point(43, 4);
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.errorLabel.Location = new System.Drawing.Point(42, 0);
             this.errorLabel.Name = "errorLabel";
-            this.errorLabel.Size = new System.Drawing.Size(480, 32);
+            this.errorLabel.Size = new System.Drawing.Size(481, 39);
             this.errorLabel.TabIndex = 1;
             this.errorLabel.Text = "Error details.";
-            this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // pictureBox1
+            // iconPictureBox
             // 
-            this.pictureBox1.Image = global::EVEMon.Properties.Resources.Error32;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.iconPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iconPictureBox.Image = global::EVEMon.Properties.Resources.Error32;
+            this.iconPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.iconPictureBox.Name = "iconPictureBox";
+            this.iconPictureBox.Size = new System.Drawing.Size(33, 33);
+            this.iconPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconPictureBox.TabIndex = 0;
+            this.iconPictureBox.TabStop = false;
             // 
-            // borderPanel1
+            // detailsPanel
             // 
-            this.borderPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.detailsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.borderPanel1.BackColor = System.Drawing.SystemColors.Window;
-            this.borderPanel1.Controls.Add(this.detailsTextBox);
-            this.borderPanel1.ForeColor = System.Drawing.Color.Gray;
-            this.borderPanel1.Location = new System.Drawing.Point(13, 61);
-            this.borderPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.borderPanel1.Name = "borderPanel1";
-            this.borderPanel1.Size = new System.Drawing.Size(526, 371);
-            this.borderPanel1.TabIndex = 2;
+            this.detailsPanel.BackColor = System.Drawing.SystemColors.Window;
+            this.detailsPanel.Controls.Add(this.detailsTextBox);
+            this.detailsPanel.ForeColor = System.Drawing.Color.Gray;
+            this.detailsPanel.Location = new System.Drawing.Point(13, 66);
+            this.detailsPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.detailsPanel.Name = "detailsPanel";
+            this.detailsPanel.Padding = new System.Windows.Forms.Padding(3);
+            this.detailsPanel.Size = new System.Drawing.Size(526, 366);
+            this.detailsPanel.TabIndex = 2;
             // 
             // detailsTextBox
             // 
-            this.detailsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.detailsTextBox.BackColor = System.Drawing.SystemColors.Window;
             this.detailsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.detailsTextBox.Location = new System.Drawing.Point(4, 4);
+            this.detailsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.detailsTextBox.Location = new System.Drawing.Point(3, 3);
             this.detailsTextBox.Multiline = true;
             this.detailsTextBox.Name = "detailsTextBox";
             this.detailsTextBox.ReadOnly = true;
-            this.detailsTextBox.Size = new System.Drawing.Size(518, 363);
+            this.detailsTextBox.Size = new System.Drawing.Size(520, 360);
             this.detailsTextBox.TabIndex = 1;
+            // 
+            // errorTableLayoutPanel
+            // 
+            this.errorTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorTableLayoutPanel.AutoSize = true;
+            this.errorTableLayoutPanel.ColumnCount = 2;
+            this.errorTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.414449F));
+            this.errorTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 92.58555F));
+            this.errorTableLayoutPanel.Controls.Add(this.errorLabel, 1, 0);
+            this.errorTableLayoutPanel.Controls.Add(this.iconPictureBox, 0, 0);
+            this.errorTableLayoutPanel.Location = new System.Drawing.Point(13, 12);
+            this.errorTableLayoutPanel.Name = "errorTableLayoutPanel";
+            this.errorTableLayoutPanel.RowCount = 1;
+            this.errorTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.errorTableLayoutPanel.Size = new System.Drawing.Size(526, 39);
+            this.errorTableLayoutPanel.TabIndex = 3;
             // 
             // APIErrorWindow
             // 
@@ -103,24 +109,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(551, 441);
-            this.Controls.Add(this.borderPanel1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.errorTableLayoutPanel);
+            this.Controls.Add(this.detailsPanel);
             this.Name = "APIErrorWindow";
             this.Text = "API Error Details";
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.borderPanel1.ResumeLayout(false);
-            this.borderPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox)).EndInit();
+            this.detailsPanel.ResumeLayout(false);
+            this.detailsPanel.PerformLayout();
+            this.errorTableLayoutPanel.ResumeLayout(false);
+            this.errorTableLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label errorLabel;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox iconPictureBox;
         private System.Windows.Forms.TextBox detailsTextBox;
-        private EVEMon.Controls.BorderPanel borderPanel1;
+        private EVEMon.Controls.BorderPanel detailsPanel;
+        private System.Windows.Forms.TableLayoutPanel errorTableLayoutPanel;
     }
 }
