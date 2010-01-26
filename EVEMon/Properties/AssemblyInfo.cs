bd4163e9466b16e94f0cd2425be6b0528cb61225
Richard Slater <richard.slater@hotmail.com>
2010-01-26 19:58:10 +0000

    1.3.0.2041

--- EVEMon/Properties/AssemblyInfo.cs
+++ EVEMon/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EVEMon")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 2040 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon")]
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 2041 Local Changes: Yes Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
@@ -32,4 +32,4 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.2040")]
+[assembly: AssemblyVersion("1.3.0.2041")]
