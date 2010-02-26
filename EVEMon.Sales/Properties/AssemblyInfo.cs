ab8270070805b7a5fe8a7fbd5c25e31c94de1466
Richard Slater <richard.slater@hotmail.com>
2010-02-26 20:40:24 +0000

    EVEMon 1.3.1.2117.

--- EVEMon.Sales/Properties/AssemblyInfo.cs
+++ EVEMon.Sales/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EVEMon.Sales")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 2098 Local Changes: No Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon.Sales")] 
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 2117 Local Changes: No Repository URL: svn://evemon.battleclinic.com/EVEMon/EVEMon/trunk/EVEMon.Sales")] 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.2098")]
+[assembly: AssemblyVersion("1.3.0.2117")]
