fdcaf4a45881c4a269b497d1e137114d2513ee2e
Richard Slater <richard.slater@hotmail.com>
2010-05-17 17:59:02 +0000

    Added missing G15Support.cs.

---
+++ EVEMon.WindowsApi/G15Support.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVEMon.WindowsApi
+{
+    public static class G15Support
+    {
+        public static void Start()
+        {
+            if (OsFeatureCheck.IsWindowsNT)
+            {
+                G15Handler.Init();
+            }
+        }
+    }
+}
