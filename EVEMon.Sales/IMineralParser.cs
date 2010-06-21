40db67bf4ac82fef704a0fc8a703a4a657c9a98a
Richard Slater <richard.slater@hotmail.com>
2010-06-21 20:12:14 +0000

    Refactoring of IMineralParser into separte files.

--- EVEMon.Sales/IMineralParser.cs
+++ EVEMon.Sales/IMineralParser.cs
@@ -5,111 +5,33 @@ using EVEMon.Common;
 
 namespace EVEMon.Sales
 {
-    public class DefaultMineralParserAttribute : Attribute
-    {
-        private string m_name = String.Empty;
-
-        public string Name
-        {
-            get { return m_name; }
-            set { m_name = value; }
-        }
-
-        public DefaultMineralParserAttribute(string name)
-        {
-            m_name = name;
-        }
-    }
-
-    public static class MineralDataRequest
-    {
-        static MineralDataRequest()
-        {
-            sm_parsers = new Dictionary<string, IMineralParser>();
-
-            Assembly asm = Assembly.GetExecutingAssembly();
-            foreach (Type tt in asm.GetTypes())
-            {
-                foreach (
-                    DefaultMineralParserAttribute dmpa in
-                        tt.GetCustomAttributes(typeof (DefaultMineralParserAttribute), false))
-                {
-                    IMineralParser mp = Activator.CreateInstance(tt) as IMineralParser;
-                    if (mp != null)
-                    {
-                        RegisterDataSource(dmpa.Name, mp);
-                    }
-                }
-            }
-        }
-
-        private static Dictionary<string, IMineralParser> sm_parsers;
-
-        public static IEnumerable<Pair<string, IMineralParser>> Parsers
-        {
-            get
-            {
-                foreach (KeyValuePair<string, IMineralParser> kvp in sm_parsers)
-                {
-                    yield return new Pair<string, IMineralParser>(kvp.Key, kvp.Value);
-                }
-            }
-        }
-
-        public static void RegisterDataSource(string name, IMineralParser parser)
-        {
-            sm_parsers.Add(name, parser);
-        }
-
-        public static IEnumerable<Pair<string, Decimal>> GetPrices(string source)
-        {
-            if (!sm_parsers.ContainsKey(source))
-            {
-                throw new ArgumentException("that is not a registered mineraldatasource", "source");
-            }
-
-            IMineralParser p = sm_parsers[source];
-            return p.GetPrices();
-        }
-
-        public static string GetCourtesyText(string source)
-        {
-            if (!sm_parsers.ContainsKey(source))
-            {
-                throw new ArgumentException("that is not a registered mineraldatasource", "source");
-            }
-
-            IMineralParser p = sm_parsers[source];
-            return p.CourtesyText;
-        }
-
-        public static string GetCourtesyUrl(string source)
-        {
-            if (!sm_parsers.ContainsKey(source))
-            {
-                throw new ArgumentException("that is not a registered mineraldatasource", "source");
-            }
-
-            IMineralParser p = sm_parsers[source];
-            return p.CourtesyUrl;
-        }
-    }
-
+    /// <summary>
+    /// Defines an interface to a Mineral Parser.
+    /// </summary>
     public interface IMineralParser
     {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <value>The title.</value>
         string Title { get; }
 
+        /// <summary>
+        /// Gets the courtesy URL.
+        /// </summary>
+        /// <value>The courtesy URL.</value>
         string CourtesyUrl { get; }
+
+        /// <summary>
+        /// Gets the courtesy text.
+        /// </summary>
+        /// <value>The courtesy text.</value>
         string CourtesyText { get; }
 
+        /// <summary>
+        /// Gets the prices.
+        /// </summary>
+        /// <returns>An enumerable collection of Minerals and Prices.</returns>
         IEnumerable<Pair<string, Decimal>> GetPrices();
-    }
-
-    public class MineralParserException : ApplicationException
-    {
-        public MineralParserException(string msg)
-            : base(msg)
-        {
-        }
     }
 }
---
+++ EVEMon.Sales/MineralParserException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using EVEMon.Common;
+
+namespace EVEMon.Sales
+{
+    /// <summary>
+    /// Thrown when something goes wrong with the mineral parser.
+    /// </summary>
+    public class MineralParserException : ApplicationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MineralParserException"/> class.
+        /// </summary>
+        /// <param name="msg">The message.</param>
+        public MineralParserException(string message)
+            : base(message)
+        {
+        }
+    }
+}
