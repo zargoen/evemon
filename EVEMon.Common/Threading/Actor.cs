23131d6bddc9ff56df97ebe3ebf03a33349791b6
Richard Slater <richard.slater@hotmail.com>
2010-06-21 19:36:49 +0000

    Refactoring to avoid private struct with public members.

--- EVEMon.Common/Threading/Actor.cs
+++ EVEMon.Common/Threading/Actor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
 using System.Threading;
-using System.Reflection;
 
 #pragma warning disable 420
 
@@ -14,65 +11,6 @@ namespace EVEMon.Common.Threading
     /// </summary>
     public sealed class Actor : IActor
     {
-        #region Message
-        /// <summary>
-        /// Encapsulates an action, also providing synchronization services
-        /// </summary>
-        private struct Message
-        {
-            private readonly Action m_action;
-            private readonly EventWaitHandle m_waitHandle;
-
-            /// <summary>
-            /// Constructor for an message without waiting mechanism
-            /// </summary>
-            /// <param name="action"></param>
-            public Message(Action action)
-            {
-                this.m_action = action;
-                this.m_waitHandle = null;
-            }
-
-            /// <summary>
-            /// Constructor for a message with or without waiting mechanism
-            /// </summary>
-            /// <param name="action"></param>
-            /// <param name="waitForInvocation">When true, the message creator requires a waiting mechanism</param>
-            public Message(Action action, bool waitForInvocation)
-            {
-                this.m_action = action;
-
-                if (waitForInvocation) this.m_waitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
-                else this.m_waitHandle = null;
-            }
-
-            /// <summary>
-            /// Invoke the action and, when appliable, signal the creator thread.
-            /// </summary>
-            public void Invoke()
-            {
-                this.m_action();
-                if (this.m_waitHandle != null) m_waitHandle.Set();
-            }
-
-            /// <summary>
-            /// Wait for the action completion by the owner of the messages pump
-            /// </summary>
-            public void Wait()
-            {
-                if (this.m_waitHandle != null) this.m_waitHandle.WaitOne();
-            }
-
-            /// <summary>
-            /// Cancels the operation, just signals the waiting thread when any
-            /// </summary>
-            public void Cancel()
-            {
-                if (this.m_waitHandle != null) m_waitHandle.Set();
-            }
-        }
-        #endregion
-
         /// <summary>
         /// Holds a reference to the actor currently associated with this thread
         /// </summary>
