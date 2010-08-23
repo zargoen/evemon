feb83c6e85ab7efec6fb90f9f3e8918d84cad937
Richard Slater <richard.slater@hotmail.com>
2010-08-23 21:23:02 +0000

    Removed unused namespace references from UIActor.

--- EVEMon.Common/Threading/UIActor.cs
+++ EVEMon.Common/Threading/UIActor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Forms;
 using System.Threading;
-using System.Reflection;
 
 #pragma warning disable 420
 
