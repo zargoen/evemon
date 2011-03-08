c63b570690dea5f9ab28e75a3135489f7e2c276f
Jimi C <jimikar@gmail.com>
2011-03-08 18:34:52 +0000

    Assembly info revision number on each project will now get the root revision number upon a release build.

--- UpdateSVNRevision.cmd
+++ UpdateSVNRevision.cmd
@@ -6,5 +6,5 @@ GOTO END
 :RELEASE
 cd /D %1
 echo DEBUG not set, building installers
-SubWCRev.exe . ".\Properties\AssemblyInfo.template.cs" ".\Properties\AssemblyInfo.cs" -f
+SubWCRev.exe .. ".\Properties\AssemblyInfo.template.cs" ".\Properties\AssemblyInfo.cs" -f
 :END
