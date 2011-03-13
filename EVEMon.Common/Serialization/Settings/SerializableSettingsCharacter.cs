b46c3167df0d14e1be8780dba90798a63a4c54ef
Jimi C <jimikar@gmail.com>
2011-03-13 09:27:21 +0000

    Refactoring: Applying coding guidelines to merged classes by splitting them as one class - one file.

--- EVEMon.Common/Serialization/Settings/SerializableSettingsCharacter.cs
+++ EVEMon.Common/Serialization/Settings/SerializableSettingsCharacter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Xml.Serialization;
 
 using EVEMon.Common.Serialization.API;
@@ -11,11 +10,6 @@ namespace EVEMon.Common.Serialization.Settings
     /// </summary>
     public class SerializableSettingsCharacter : SerializableCharacterSheetBase
     {
-        public SerializableSettingsCharacter()
-        {
-            this.ImplantSets = new SerializableImplantSetCollection();
-        }
-
         [XmlAttribute("guid")]
         public Guid Guid
         {
@@ -29,101 +23,5 @@ namespace EVEMon.Common.Serialization.Settings
             get;
             set;
         }
-    }
-
-    /// <summary>
-    /// Represents a serializable character defined from an uri file
-    /// </summary>
-    public sealed class SerializableUriCharacter : SerializableSettingsCharacter
-    {
-        [XmlElement("uri")]
-        public string Uri
-        {
-            get;
-            set;
-        }
-    }
-
-    /// <summary>
-    /// Represents a serializable character defined from the API
-    /// </summary>
-    public sealed class SerializableCCPCharacter : SerializableSettingsCharacter
-    {
-        public SerializableCCPCharacter()
-        {
-            SkillQueue = new List<SerializableQueuedSkill>();
-            LastUpdates = new List<SerializableAPIUpdate>();
-            MarketOrders = new List<SerializableOrderBase>();
-            IndustryJobs = new List<SerializableJob>();
-            ResearchPoints = new List<SerializableResearchPoint>();
-        }
-
-        [XmlArray("queue")]
-        [XmlArrayItem("skill")]
-        public List<SerializableQueuedSkill> SkillQueue
-        {
-            get;
-            set;
-        }
-
-        [XmlArray("marketOrders")]
-        [XmlArrayItem("buy", typeof(SerializableBuyOrder))]
-        [XmlArrayItem("sell", typeof(SerializableSellOrder))]
-        public List<SerializableOrderBase> MarketOrders
-        {
-            get;
-            set;
-        }
-
-        [XmlArray("industryJobs")]
-        [XmlArrayItem("job")]
-        public List<SerializableJob> IndustryJobs
-        {
-            get;
-            set;
-        }
-
-        [XmlArray("researchPoints")]
-        [XmlArrayItem("researchPoint")]
-        public List<SerializableResearchPoint> ResearchPoints
-        {
-            get;
-            set;
-        }
-
-        [XmlArray("lastUpdates")]
-        [XmlArrayItem("apiUpdate")]
-        public List<SerializableAPIUpdate> LastUpdates
-        {
-            get;
-            set;
-        }
-
-    }
-
-    /// <summary>
-    /// Represents an API method and the last time we updated it from CCP.
-    /// </summary>
-    public sealed class SerializableAPIUpdate
-    {
-        [XmlAttribute("method")]
-        public APIMethods Method
-        {
-            get;
-            set;
-        }
-
-        [XmlAttribute("time")]
-        public DateTime Time
-        {
-            get;
-            set;
-        }
-
-        public SerializableAPIUpdate Clone()
-        {
-            return (SerializableAPIUpdate)MemberwiseClone();
-        }
-
     }
 }
