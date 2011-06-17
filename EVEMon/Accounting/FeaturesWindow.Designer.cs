903ee0bece0bbb0740b93d2a34b7e9a26f28ca6f
Jimi C <jimikar@gmail.com>
2011-06-17 11:10:37 +0000

    Misc: Updating features entries in FeaturesWindow.

--- EVEMon/Accounting/FeaturesWindow.Designer.cs
+++ EVEMon/Accounting/FeaturesWindow.Designer.cs
@@ -35,7 +35,12 @@
             this.FullApiLabel = new System.Windows.Forms.Label();
             this.MarketOrdersLabel = new System.Windows.Forms.Label();
             this.IndustryJobsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fullAPIFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.MainFlowLayoutPanel.SuspendLayout();
+            this.fullAPIFlowLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // LimitedApiLabel
@@ -58,12 +63,11 @@
             this.MainFlowLayoutPanel.Controls.Add(this.CharacterLabel);
             this.MainFlowLayoutPanel.Controls.Add(this.SkillQueueLabel);
             this.MainFlowLayoutPanel.Controls.Add(this.FullApiLabel);
-            this.MainFlowLayoutPanel.Controls.Add(this.MarketOrdersLabel);
-            this.MainFlowLayoutPanel.Controls.Add(this.IndustryJobsLabel);
+            this.MainFlowLayoutPanel.Controls.Add(this.fullAPIFlowLayoutPanel);
             this.MainFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.MainFlowLayoutPanel.Location = new System.Drawing.Point(12, 12);
             this.MainFlowLayoutPanel.Name = "MainFlowLayoutPanel";
-            this.MainFlowLayoutPanel.Size = new System.Drawing.Size(342, 116);
+            this.MainFlowLayoutPanel.Size = new System.Drawing.Size(342, 144);
             this.MainFlowLayoutPanel.TabIndex = 1;
             // 
             // CharacterLabel
@@ -82,9 +86,9 @@
             this.SkillQueueLabel.Location = new System.Drawing.Point(3, 34);
             this.SkillQueueLabel.Name = "SkillQueueLabel";
             this.SkillQueueLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.SkillQueueLabel.Size = new System.Drawing.Size(224, 18);
+            this.SkillQueueLabel.Size = new System.Drawing.Size(232, 18);
             this.SkillQueueLabel.TabIndex = 1;
-            this.SkillQueueLabel.Text = "Skill queue monitoring and skills planification.";
+            this.SkillQueueLabel.Text = "Skill queue monitoring and skill levels planning.";
             // 
             // FullApiLabel
             // 
@@ -101,22 +105,65 @@
             // MarketOrdersLabel
             // 
             this.MarketOrdersLabel.AutoSize = true;
-            this.MarketOrdersLabel.Location = new System.Drawing.Point(3, 78);
+            this.MarketOrdersLabel.Location = new System.Drawing.Point(3, 0);
             this.MarketOrdersLabel.Name = "MarketOrdersLabel";
             this.MarketOrdersLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.MarketOrdersLabel.Size = new System.Drawing.Size(132, 18);
+            this.MarketOrdersLabel.Size = new System.Drawing.Size(136, 18);
             this.MarketOrdersLabel.TabIndex = 3;
-            this.MarketOrdersLabel.Text = "Market orders monitoring";
+            this.MarketOrdersLabel.Text = "Market orders monitoring.";
             // 
             // IndustryJobsLabel
             // 
             this.IndustryJobsLabel.AutoSize = true;
-            this.IndustryJobsLabel.Location = new System.Drawing.Point(3, 96);
+            this.IndustryJobsLabel.Location = new System.Drawing.Point(3, 18);
             this.IndustryJobsLabel.Name = "IndustryJobsLabel";
             this.IndustryJobsLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.IndustryJobsLabel.Size = new System.Drawing.Size(129, 18);
+            this.IndustryJobsLabel.Size = new System.Drawing.Size(133, 18);
             this.IndustryJobsLabel.TabIndex = 5;
-            this.IndustryJobsLabel.Text = "Industry jobs monitoring";
+            this.IndustryJobsLabel.Text = "Industry jobs monitoring.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 36);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.label1.Size = new System.Drawing.Size(146, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Research points monitoring.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.label2.Size = new System.Drawing.Size(158, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "EVE Mail messages monitoring.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(155, 18);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.label3.Size = new System.Drawing.Size(149, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "EVE Notifications monitoring.";
+            // 
+            // fullAPIFlowLayoutPanel
+            // 
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.MarketOrdersLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.IndustryJobsLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.label1);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.label2);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.label3);
+            this.fullAPIFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.fullAPIFlowLayoutPanel.Location = new System.Drawing.Point(3, 81);
+            this.fullAPIFlowLayoutPanel.Name = "fullAPIFlowLayoutPanel";
+            this.fullAPIFlowLayoutPanel.Size = new System.Drawing.Size(339, 60);
+            this.fullAPIFlowLayoutPanel.TabIndex = 10;
             // 
             // FeaturesWindow
             // 
@@ -125,7 +172,7 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(366, 140);
+            this.ClientSize = new System.Drawing.Size(366, 168);
             this.Controls.Add(this.MainFlowLayoutPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -136,6 +183,8 @@
             this.TopMost = true;
             this.MainFlowLayoutPanel.ResumeLayout(false);
             this.MainFlowLayoutPanel.PerformLayout();
+            this.fullAPIFlowLayoutPanel.ResumeLayout(false);
+            this.fullAPIFlowLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -149,5 +198,9 @@
         private System.Windows.Forms.Label MarketOrdersLabel;
         private System.Windows.Forms.Label CharacterLabel;
         private System.Windows.Forms.Label IndustryJobsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel fullAPIFlowLayoutPanel;
     }
 }
