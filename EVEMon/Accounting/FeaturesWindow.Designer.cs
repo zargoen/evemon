c873917a19964a6894557c6de5cf143df03f5880
Jimi C <jimikar@gmail.com>
2011-07-10 08:17:04 +0000

    Misc: Updating features entries in FeaturesWindow.

--- EVEMon/Accounting/FeaturesWindow.Designer.cs
+++ EVEMon/Accounting/FeaturesWindow.Designer.cs
@@ -33,12 +33,13 @@
             this.CharacterLabel = new System.Windows.Forms.Label();
             this.SkillQueueLabel = new System.Windows.Forms.Label();
             this.FullApiLabel = new System.Windows.Forms.Label();
+            this.fullAPIFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.MarketOrdersLabel = new System.Windows.Forms.Label();
             this.IndustryJobsLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.fullAPIFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.ResearchPointsLabel = new System.Windows.Forms.Label();
+            this.EVEMailMessagesLabel = new System.Windows.Forms.Label();
+            this.EVENotificationsLabel = new System.Windows.Forms.Label();
+            this.AccountStatusLabel = new System.Windows.Forms.Label();
             this.MainFlowLayoutPanel.SuspendLayout();
             this.fullAPIFlowLayoutPanel.SuspendLayout();
             this.SuspendLayout();
@@ -102,10 +103,24 @@
             this.FullApiLabel.TabIndex = 2;
             this.FullApiLabel.Text = "Full API Key";
             // 
+            // fullAPIFlowLayoutPanel
+            // 
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.AccountStatusLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.EVEMailMessagesLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.EVENotificationsLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.IndustryJobsLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.MarketOrdersLabel);
+            this.fullAPIFlowLayoutPanel.Controls.Add(this.ResearchPointsLabel);
+            this.fullAPIFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.fullAPIFlowLayoutPanel.Location = new System.Drawing.Point(3, 81);
+            this.fullAPIFlowLayoutPanel.Name = "fullAPIFlowLayoutPanel";
+            this.fullAPIFlowLayoutPanel.Size = new System.Drawing.Size(339, 60);
+            this.fullAPIFlowLayoutPanel.TabIndex = 10;
+            // 
             // MarketOrdersLabel
             // 
             this.MarketOrdersLabel.AutoSize = true;
-            this.MarketOrdersLabel.Location = new System.Drawing.Point(3, 0);
+            this.MarketOrdersLabel.Location = new System.Drawing.Point(167, 18);
             this.MarketOrdersLabel.Name = "MarketOrdersLabel";
             this.MarketOrdersLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
             this.MarketOrdersLabel.Size = new System.Drawing.Size(136, 18);
@@ -115,55 +130,52 @@
             // IndustryJobsLabel
             // 
             this.IndustryJobsLabel.AutoSize = true;
-            this.IndustryJobsLabel.Location = new System.Drawing.Point(3, 18);
+            this.IndustryJobsLabel.Location = new System.Drawing.Point(167, 0);
             this.IndustryJobsLabel.Name = "IndustryJobsLabel";
             this.IndustryJobsLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
             this.IndustryJobsLabel.Size = new System.Drawing.Size(133, 18);
             this.IndustryJobsLabel.TabIndex = 5;
             this.IndustryJobsLabel.Text = "Industry jobs monitoring.";
             // 
-            // label1
+            // ResearchPointsLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 36);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.label1.Size = new System.Drawing.Size(146, 18);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Research points monitoring.";
+            this.ResearchPointsLabel.AutoSize = true;
+            this.ResearchPointsLabel.Location = new System.Drawing.Point(167, 36);
+            this.ResearchPointsLabel.Name = "ResearchPointsLabel";
+            this.ResearchPointsLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.ResearchPointsLabel.Size = new System.Drawing.Size(146, 18);
+            this.ResearchPointsLabel.TabIndex = 6;
+            this.ResearchPointsLabel.Text = "Research points monitoring.";
             // 
-            // label2
+            // EVEMailMessagesLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(155, 0);
-            this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.label2.Size = new System.Drawing.Size(158, 18);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "EVE Mail messages monitoring.";
+            this.EVEMailMessagesLabel.AutoSize = true;
+            this.EVEMailMessagesLabel.Location = new System.Drawing.Point(3, 18);
+            this.EVEMailMessagesLabel.Name = "EVEMailMessagesLabel";
+            this.EVEMailMessagesLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.EVEMailMessagesLabel.Size = new System.Drawing.Size(158, 18);
+            this.EVEMailMessagesLabel.TabIndex = 7;
+            this.EVEMailMessagesLabel.Text = "EVE Mail messages monitoring.";
             // 
-            // label3
+            // EVENotificationsLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(155, 18);
-            this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.label3.Size = new System.Drawing.Size(149, 18);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "EVE Notifications monitoring.";
+            this.EVENotificationsLabel.AutoSize = true;
+            this.EVENotificationsLabel.Location = new System.Drawing.Point(3, 36);
+            this.EVENotificationsLabel.Name = "EVENotificationsLabel";
+            this.EVENotificationsLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.EVENotificationsLabel.Size = new System.Drawing.Size(149, 18);
+            this.EVENotificationsLabel.TabIndex = 8;
+            this.EVENotificationsLabel.Text = "EVE Notifications monitoring.";
             // 
-            // fullAPIFlowLayoutPanel
+            // AccountStatusLabel
             // 
-            this.fullAPIFlowLayoutPanel.Controls.Add(this.MarketOrdersLabel);
-            this.fullAPIFlowLayoutPanel.Controls.Add(this.IndustryJobsLabel);
-            this.fullAPIFlowLayoutPanel.Controls.Add(this.label1);
-            this.fullAPIFlowLayoutPanel.Controls.Add(this.label2);
-            this.fullAPIFlowLayoutPanel.Controls.Add(this.label3);
-            this.fullAPIFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.fullAPIFlowLayoutPanel.Location = new System.Drawing.Point(3, 81);
-            this.fullAPIFlowLayoutPanel.Name = "fullAPIFlowLayoutPanel";
-            this.fullAPIFlowLayoutPanel.Size = new System.Drawing.Size(339, 60);
-            this.fullAPIFlowLayoutPanel.TabIndex = 10;
+            this.AccountStatusLabel.AutoSize = true;
+            this.AccountStatusLabel.Location = new System.Drawing.Point(3, 0);
+            this.AccountStatusLabel.Name = "AccountStatusLabel";
+            this.AccountStatusLabel.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.AccountStatusLabel.Size = new System.Drawing.Size(142, 18);
+            this.AccountStatusLabel.TabIndex = 9;
+            this.AccountStatusLabel.Text = "Account Status monitoring.";
             // 
             // FeaturesWindow
             // 
@@ -198,9 +210,10 @@
         private System.Windows.Forms.Label MarketOrdersLabel;
         private System.Windows.Forms.Label CharacterLabel;
         private System.Windows.Forms.Label IndustryJobsLabel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ResearchPointsLabel;
+        private System.Windows.Forms.Label EVEMailMessagesLabel;
+        private System.Windows.Forms.Label EVENotificationsLabel;
         private System.Windows.Forms.FlowLayoutPanel fullAPIFlowLayoutPanel;
+        private System.Windows.Forms.Label AccountStatusLabel;
     }
 }
