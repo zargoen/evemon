cb1f74ea0338b87e9be8904d2bb05716422f0167
Jimi C <jimikar@gmail.com>
2011-08-27 14:12:54 +0000

    Misc: Reverting false deletion in previous commit.

---
+++ EVEMon.Common/Collections/Interfaces.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace EVEMon.Common.Collections
+{
+
+
+    #region IReadonlyCollection<T>
+
+    /// <summary>
+    /// Represents a read-only collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReadonlyCollection<out T> : IEnumerable<T>
+    {
+        int Count { get; }
+    }
+
+    #endregion
+
+
+    #region IReadonlyKeyedCollection<T>
+
+    /// <summary>
+    /// Represents a read-only collection
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TItem">The type of the item.</typeparam>
+    public interface IReadonlyKeyedCollection<in TKey, out TItem> : IEnumerable<TItem>
+    {
+        int Count { get; }
+        TItem this[TKey key] { get; }
+    }
+
+    #endregion
+}
