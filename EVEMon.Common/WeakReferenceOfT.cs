c4f770218a4d1367b0c8efa6b2f10e46df3fc6dc
Jimi C <jimikar@gmail.com>
2011-11-08 11:03:41 +0200

    Refactoring: Applying rule CA2229 (Implement serialization constructors) to project EVEMon.Common.

--- EVEMon.Common/WeakReferenceOfT.cs
+++ EVEMon.Common/WeakReferenceOfT.cs
@@ -1,16 +1,39 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace EVEMon.Common
 {
     [Serializable]
-    public class WeakReference<T> : WeakReference
+    public sealed class WeakReference<T> : WeakReference
         where T : class
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakReference&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="info">An object that holds all the data needed to serialize or deserialize the current <see cref="T:System.WeakReference"/> object.</param>
+        /// <param name="context">(Reserved) Describes the source and destination of the serialized stream specified by <paramref name="info"/>.</param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// 	<paramref name="info"/> is null. </exception>
+        /// <remarks>Implemented to satisfy rule CA2229</remarks>
+        private WeakReference(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakReference&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The target.</param>
         public WeakReference(T target)
             : base(target)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeakReference&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="trackResurrection">if set to <c>true</c> [track resurrection].</param>
         public WeakReference(T target, bool trackResurrection)
             : base(target, trackResurrection)
         {
