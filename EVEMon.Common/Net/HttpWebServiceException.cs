d28d208b84225c99799aac2e92b555ab1cc30d29
Jimi C <jimikar@gmail.com>
2011-11-20 10:27:36 +0200

    Refactoring: Applying rule CA2240 (Implement ISerializable correctly).

--- EVEMon.Common/Net/HttpWebServiceException.cs
+++ EVEMon.Common/Net/HttpWebServiceException.cs
@@ -11,20 +11,39 @@ namespace EVEMon.Common.Net
     [Serializable]
     public sealed class HttpWebServiceException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpWebServiceException"/> class.
+        /// </summary>
         public HttpWebServiceException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpWebServiceException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
         public HttpWebServiceException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpWebServiceException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="ex">The ex.</param>
         public HttpWebServiceException(string message, Exception ex)
             : base(message, ex)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpWebServiceException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
+        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
         private HttpWebServiceException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
@@ -254,6 +273,28 @@ namespace EVEMon.Common.Net
             return new HttpWebServiceException(HttpWebServiceExceptionStatus.FileError, ex, url,
                                                String.Format(CultureConstants.DefaultCulture,
                                                              ExceptionMessages.FileException, url.Host));
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, sets the <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with information about the exception.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is a null reference (Nothing in Visual Basic). </exception>
+        /// <PermissionSet>
+        /// 	<IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Read="*AllFiles*" PathDiscovery="*AllFiles*"/>
+        /// 	<IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="SerializationFormatter"/>
+        /// </PermissionSet>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException("info");
+
+            info.AddValue("Status", Status);
+            info.AddValue("Url", Url);
+            info.AddValue("HostName", HostName);
+
+            base.GetObjectData(info, context);
         }
     }
 }
