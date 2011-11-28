d2edf1be2bba897bf4a7863f95d4808e5d1c0576
Jimi C <jimikar@gmail.com>
2011-11-28 10:46:32 +0200

    Refactoring: Simplifying code by use of LINQ.

--- EVEMon.Sales/CXIParser.cs
+++ EVEMon.Sales/CXIParser.cs
@@ -3,14 +3,17 @@ using System.Text.RegularExpressions;
 
 namespace EVEMon.Sales
 {
-    [DefaultMineralParser("cxi")]
     public class CXIParser : IMineralParser
     {
         /// <summary>
-        /// Gets or sets the name.
+        /// Gets  the name.
         /// </summary>
-        /// <value>The anme.</value>
-        public string Name { get; set; }
+        /// <value>The name.</value>
+        public string Name
+        {
+            get { return "cxi"; }
+        }
+
 
         /// <summary>
         /// Gets the title.
