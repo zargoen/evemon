c38b802cf8fdb68356494bed4af4300a436e9344
Jimi C <jimikar@gmail.com>
2011-12-10 20:33:43 +0200

    Release of EVEMon 1.5.4.

--- EVEMon.Watchdog/Properties/AssemblyInfo.template.cs
+++ EVEMon.Watchdog/Properties/AssemblyInfo.template.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.3.$REVNUM$")]
+[assembly: AssemblyVersion("1.5.4.$REVNUM$")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
