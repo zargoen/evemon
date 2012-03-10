d02124a4b538841ac338ed534b49e19e52b37de3
Jimi C <jimikar@gmail.com>
2012-03-10 17:04:22 +0200

    Release of EVEMon 1.6.0

--- EVEMon.PieChart/Properties/AssemblyInfo.template.cs
+++ EVEMon.PieChart/Properties/AssemblyInfo.template.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.4.$REVNUM$")]
+[assembly: AssemblyVersion("1.6.0.$REVNUM$")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
