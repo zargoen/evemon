c0b308ace086f7e3524e06a33825084cec8cd9cc
Jimi C <jimikar@gmail.com>
2012-06-10 17:28:20 +0300

    Release of EVEMon 1.7.0.

--- EVEMon.Common/Properties/AssemblyInfo.template.cs
+++ EVEMon.Common/Properties/AssemblyInfo.template.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.1.$REVNUM$")]
+[assembly: AssemblyVersion("1.7.0.$REVNUM$")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
