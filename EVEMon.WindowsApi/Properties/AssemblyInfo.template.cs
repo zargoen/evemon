ba72fd670c70de8fdd86165493d96f97226f9766
Jimi C <jimikar@gmail.com>
2012-10-26 22:33:00 +0300

    Misc: Updating Assembly templates to use new MSBuildVersioning token UTCYEAR.

--- EVEMon.WindowsApi/Properties/AssemblyInfo.template.cs
+++ EVEMon.WindowsApi/Properties/AssemblyInfo.template.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
-[assembly: AssemblyCopyright("Copyright © 2010-2012, EVEMon Development Team")]
+[assembly: AssemblyCopyright("Copyright © 2010-$UTCYEAR$, EVEMon Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
