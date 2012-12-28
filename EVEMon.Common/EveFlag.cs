94d93c7072a51fb1eaa19964965ad106ff95c76c
Jimi C <jimikar@gmail.com>
2012-12-28 10:10:02 +0200

    Refactoring: Refactoring deserialazation code of EveFlag.

--- EVEMon.Common/EveFlag.cs
+++ EVEMon.Common/EveFlag.cs
@@ -22,7 +22,9 @@ namespace EVEMon.Common
         {
             EnsureInitialized();
 
-            SerializableEveFlagsListItem flag = s_eveFlags.EVEFlags.FirstOrDefault(x => x.ID == id);
+            SerializableEveFlagsListItem flag = s_eveFlags != null
+                                                    ? s_eveFlags.EVEFlags.FirstOrDefault(x => x.ID == id)
+                                                    : null;
             return flag != null ? flag.Text : "Unknown";
         }
 
@@ -34,30 +36,8 @@ namespace EVEMon.Common
             if (s_isLoaded)
                 return;
 
-            // Read the resource file
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(Properties.Resources.Flags);
-
-            // Read the nodes
-            using (XmlNodeReader reader = new XmlNodeReader(xmlDoc))
-            {
-                // Create a memory stream to transform the xml 
-                MemoryStream stream = Util.GetMemoryStream();
-
-                // Write the xml output to the stream
-                using (XmlTextWriter writer = new XmlTextWriter(stream, Encoding.UTF8))
-                {
-                    // Apply the XSL transform
-                    writer.Formatting = Formatting.Indented;
-                    APIProvider.RowsetsTransform.Transform(reader, writer);
-                    writer.Flush();
-
-                    // Deserialize from the given stream
-                    stream.Seek(0, SeekOrigin.Begin);
-                    XmlSerializer xs = new XmlSerializer(typeof(SerializableEveFlags));
-                    s_eveFlags = (SerializableEveFlags)xs.Deserialize(stream);
-                }
-            }
+            // Deserialize the resource file
+            s_eveFlags = Util.DeserializeXmlFromString<SerializableEveFlags>(Properties.Resources.Flags, APIProvider.RowsetsTransform);
 
             s_isLoaded = true;
         }
