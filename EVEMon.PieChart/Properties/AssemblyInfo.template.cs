1f33eb7ea1281491f8869ea78de8fb3ee087957d
Jimi C <jimikar@gmail.com>
2013-06-04 20:02:06 +0300

    Release of EVEMon 1.8.2.

--- EVEMon.PieChart/Properties/AssemblyInfo.template.cs
+++ EVEMon.PieChart/Properties/AssemblyInfo.template.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.1.$REVNUM$")]
+[assembly: AssemblyVersion("1.8.2.$REVNUM$")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
