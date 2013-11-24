2edadad9e0986ae3d7250a8940e9d71b1335fe5f
Jimi C <jimikar@gmail.com>
2013-11-24 21:35:25 +0200

    Release of EVEMon 1.8.4.

--- EVEMon.Common/Properties/AssemblyInfo.cs
+++ EVEMon.Common/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EVEMon.Common")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 4117 Repository URL: https://bitbucket.org/BattleClinic/evemon")]
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 4123 Repository URL: https://bitbucket.org/BattleClinic/evemon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.3.4117")]
+[assembly: AssemblyVersion("1.8.4.4123")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
