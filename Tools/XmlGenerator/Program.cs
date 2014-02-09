a5f4117a9dff6ba28ff5d317e5e71a4e28096db6
Jimi C <jimikar@gmail.com>
2014-02-09 12:29:21 +0200

    Reverting commented out code.

--- Tools/XmlGenerator/Program.cs
+++ Tools/XmlGenerator/Program.cs
@@ -26,19 +26,19 @@ namespace EVEMon.XmlGenerator
             Console.WriteLine();
 
             // Generate datafiles
-            //Properties.GenerateDatafile();
-            //Items.GenerateDatafile(); // Requires GenerateProperties()
-            //Skills.GenerateDatafile();
+            Properties.GenerateDatafile();
+            Items.GenerateDatafile(); // Requires GenerateProperties()
+            Skills.GenerateDatafile();
             Certificates.GenerateDatafile();
-            //Blueprints.GenerateDatafile();
-            //Geography.GenerateDatafile();
-            //Reprocessing.GenerateDatafile(); // Requires GenerateItems()
+            Blueprints.GenerateDatafile();
+            Geography.GenerateDatafile();
+            Reprocessing.GenerateDatafile(); // Requires GenerateItems()
 
             // Generate MD5 Sums file
             Util.CreateMD5SumsFile("MD5Sums.txt");
 
             // Generate support xml files
-            //Flags.GenerateXMLfile();
+            Flags.GenerateXMLfile();
 
             Console.WriteLine();
             Console.WriteLine(String.Format(CultureInfo.CurrentCulture, "Generating files completed in {0}",
