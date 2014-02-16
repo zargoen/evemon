c6bb91f5905fcf29fb3a4c6d66941cb6c8ae8a79
Jimi C <jimikar@gmail.com>
2014-02-16 12:30:11 +0200

    Minor refactoring on Assembly info files.

--- Tools/YamlToSql/Properties/AssemblyInfo.cs
+++ Tools/YamlToSql/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("YamlToSql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,11 +17,9 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("a195fc16-f468-43a1-b1cd-3a356ff843ae")]
 
 // Version information for an assembly consists of the following four values:
@@ -35,7 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
