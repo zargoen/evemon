9a48f4f204332a33ad0e67acf970005d371faaa6
Jimi C <jimikar@gmail.com>
2014-03-16 10:36:13 +0200

    Updating AssemblyInfo and patch.xml files.
    Former-commit-id: 03dbeeaeec74d22a4d8bd94bbfc8409592bae8ad

--- EVEMon.PieChart/Properties/AssemblyInfo.cs
+++ EVEMon.PieChart/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.6.4263")]
+[assembly: AssemblyVersion("1.8.7.4273")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
