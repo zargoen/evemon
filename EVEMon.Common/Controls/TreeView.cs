290df82e9a6add30e910a852885309e75105ce5c
Jimi C <jimikar@gmail.com>
2014-03-29 16:40:05 +0200

    Fixed a crash bug when shift selecting in Items browser without first selecting a node.
    Former-commit-id: 89b34fe013a8d37554f8a566fe5d5c8eb0c6113d

--- EVEMon.Common/Controls/TreeView.cs
+++ EVEMon.Common/Controls/TreeView.cs
@@ -626,6 +626,9 @@ namespace EVEMon.Common.Controls
         /// <param name="tva">Specifies the action that caused the selection change.</param>
         private void UnselectNodesOutsideRange(TreeNode startNode, TreeNode endNode, TreeViewAction tva)
         {
+            if (startNode == null || endNode == null)
+                return;
+
             // Calculate start node and end node
             TreeNode firstNode;
             TreeNode lastNode;
