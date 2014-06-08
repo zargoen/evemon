a84aefb4d5ceb61599e3274cb5a15e71ff60478e
Jimi C <jimikar@gmail.com>
2014-06-08 16:59:37 +0300

    Updating AssemblyInfo and patch.xml files.
    Former-commit-id: cb9ffc45850bc719a4c95a98b6c6b339f36a480e

--- EVEMon/Properties/AssemblyInfo.cs
+++ EVEMon/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EVEMon")]
-[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 4290 Repository URL: https://bitbucket.org/BattleClinic/evemon")]
+[assembly: AssemblyDescription("EVEMon Offline Character Utility. Revision Number: 4357 Repository URL: https://bitbucket.org/BattleClinic/evemon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.8.4290")]
+[assembly: AssemblyVersion("1.8.9.4357")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
