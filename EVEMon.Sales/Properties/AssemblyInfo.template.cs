8cd827e57edef4d67406afbd0c8ad28f79aa7d36
Jimi C <jimikar@gmail.com>
2014-11-09 20:57:46 +0200

    Release of EVEMon 1.9.1.
    Former-commit-id: c635bd96a251abf0395c9ebe9c3af5a29ed28f72

--- EVEMon.Sales/Properties/AssemblyInfo.template.cs
+++ EVEMon.Sales/Properties/AssemblyInfo.template.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.9.0.$REVNUM$")]
+[assembly: AssemblyVersion("1.9.1.$REVNUM$")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
