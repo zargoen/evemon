06c8a067b4c0485da0b370394909014266554280
Jimi C <jimikar@gmail.com>
2015-07-26 18:26:33 +0300

    Replacing NUnit with xUnit.
    Former-commit-id: bbaa5507b89956745a4950375797763d7c216c69

--- Tests.EVEMon/Common/CompressionUncompressionTests.cs
+++ Tests.EVEMon/Common/CompressionUncompressionTests.cs
@@ -2,11 +2,10 @@
 using System.Linq;
 using System.Text;
 using EVEMon.Common;
-using NUnit.Framework;
+using Xunit;
 
 namespace Tests.EVEMon.Common
 {
-    [TestFixture]
     public static class CompressionUncompressionTests
     {
         private static readonly byte[] s_data = Encoding.UTF8.GetBytes("Data to compressed and uncompressed.");
@@ -17,73 +16,73 @@ namespace Tests.EVEMon.Common
         /// <summary>
         /// Compress and uncompress using zlib gzip.
         /// </summary>
-        [Test]
+        [Fact]
         public static void GzipCompressAndUncompress()
         {
             var compressed = Util.GZipCompress(s_data);
             var uncompressed = Util.GZipUncompress(compressed.ToArray());
 
-            Assert.AreEqual(uncompressed, s_data);
+            Assert.Equal(uncompressed, s_data);
         }
 
         /// <summary>
         /// Compress and uncompress using zlib deflate-inflate.
         /// </summary>
-        [Test]
+        [Fact]
         public static void DeflateCompressAndInflateUncompress()
         {
             var compressed = Util.DeflateCompress(s_data);
             var uncompressed = Util.InflateUncompress(compressed.ToArray());
 
-            Assert.AreEqual(uncompressed, s_data);
+            Assert.Equal(uncompressed, s_data);
         }
 
         /// <summary>
         /// Compress using zlib gzip and uncompress using zlib uncompress automation.
         /// </summary>
-        [Test]
+        [Fact]
         public static void GzipCompressAndZlibUncompress()
         {
             var compressed = Util.GZipCompress(s_data);
             var uncompressed = Util.ZlibUncompress(compressed.ToArray());
 
-            Assert.AreEqual(uncompressed, s_data);
+            Assert.Equal(uncompressed, s_data);
         }
 
         /// <summary>
         /// Compress using zlib deflate and uncompress using zlib uncompress automation.
         /// </summary>
-        [Test]
+        [Fact]
         public static void DeflateCompressAndZlibUncompress()
         {
             var compressed = Util.DeflateCompress(s_data);
             var uncompressed = Util.ZlibUncompress(compressed.ToArray());
 
-            Assert.AreEqual(uncompressed, s_data);
+            Assert.Equal(uncompressed, s_data);
         }
 
         /// <summary>
         /// Compress using zlib gzip and uncompress using zlib uncompress automation with stream.
         /// </summary>
-        [Test]
+        [Fact]
         public static void GzipCompressAndZlibUncompressUsingStream()
         {
             var compressed = (byte[])Util.GZipCompress(s_data);
             var uncompressed = (MemoryStream)Util.ZlibUncompress(new MemoryStream(compressed));
 
-            Assert.AreEqual(uncompressed.ToArray(), s_data);
+            Assert.Equal(uncompressed.ToArray(), s_data);
         }
 
         /// <summary>
         /// Compress using zlib deflate and uncompress using zlib uncompress automation with stream.
         /// </summary>
-        [Test]
+        [Fact]
         public static void DeflateCompressAndZlibUncompressUsingStream()
         {
             var compressed = (byte[])Util.DeflateCompress(s_data);
             var uncompressed = (MemoryStream)Util.ZlibUncompress(new MemoryStream(compressed));
 
-            Assert.AreEqual(uncompressed.ToArray(), s_data);
+            Assert.Equal(uncompressed.ToArray(), s_data);
         }
 
         #endregion
--- Tests.EVEMon/Common/TimeExtensionsTests.cs
+++ Tests.EVEMon/Common/TimeExtensionsTests.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using EVEMon.Common;
-using NUnit.Framework;
+using Xunit;
 
 namespace Tests.EVEMon.Common
 {
-    [TestFixture]
     public static class TimeExtensionsTests
     {
         #region Helper Variables
@@ -49,160 +48,160 @@ namespace Tests.EVEMon.Common
         /// <summary>
         /// Able to convert a <c>DateTime</c> to a CCPTime.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ConvertDateTimeToCCPDateTime()
         {
             string result = PointInTime.DateTimeToTimeString();
-            Assert.AreEqual(ValidCcpDateTime, result);
+            Assert.Equal(ValidCcpDateTime, result);
         }
 
         /// <summary>
         /// Able to convert a correctly formatted CCPDateTime to <c>DateTime</c>.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ConvertValidCCPDateTimeToDateTime()
         {
             DateTime result = ValidCcpDateTime.TimeStringToDateTime();
-            Assert.AreEqual(PointInTime, result);
+            Assert.Equal(PointInTime, result);
         }
 
         /// <summary>
         /// Handles an incorrect input string appropiately.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ConvertInvalidCCPDateTimeToDateTime()
         {
             DateTime result = InvalidCcpDateTime.TimeStringToDateTime();
-            Assert.AreEqual(default(DateTime), result);
+            Assert.Equal(default(DateTime), result);
         }
 
         /// <summary>
         /// Handles an empty string by returning DateTime.MinValue.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ConvertEmptyCCPDateTimeToDateTime()
         {
             DateTime result = String.Empty.TimeStringToDateTime();
-            Assert.AreEqual(DateTime.MinValue, result);
+            Assert.Equal(DateTime.MinValue, result);
         }
 
         /// <summary>
         /// Able to convert a <c>DateTime</c> to a dot formatted date/time string.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ConvertDateTimeToDotFormattedString()
         {
             string result = PointInTime.DateTimeToDotFormattedString();
-            Assert.AreEqual(ValidDotFormattedDateTimeString, result);
+            Assert.Equal(ValidDotFormattedDateTimeString, result);
         }
 
         /// <summary>
         /// If the time being tested is in the past expect "Done" to be returned.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsDone()
         {
             string result = DateTime.Now.AddHours(-1).ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("Done", result);
+            Assert.Equal("Done", result);
         }
 
         /// <summary>
-        /// Test 1s is returned when there is 1 minute to go.
+        /// Fact 1s is returned when there is 1 minute to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsSecond()
         {
             DateTime future = DateTime.Now.AddSeconds(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1s", result);
+            Assert.Equal("1s", result);
         }
 
         /// <summary>
-        /// Test 1m is returned when there is 1 minute to go.
+        /// Fact 1m is returned when there is 1 minute to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsMinute()
         {
             DateTime future = DateTime.Now.AddMinutes(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1m ", result);
+            Assert.Equal("1m ", result);
         }
 
         /// <summary>
-        /// Test 1h is returned when there is 1 hour to go.
+        /// Fact 1h is returned when there is 1 hour to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsHour()
         {
             DateTime future = DateTime.Now.AddHours(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1h ", result);
+            Assert.Equal("1h ", result);
         }
 
         /// <summary>
-        /// Test 1d is returned when there is 1 day to go.
+        /// Fact 1d is returned when there is 1 day to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsDay()
         {
             DateTime future = DateTime.Now.AddDays(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1d ", result);
+            Assert.Equal("1d ", result);
         }
 
         /// <summary>
-        /// Test 1m 1s is returned when there is 1 minute, 1 second to go.
+        /// Fact 1m 1s is returned when there is 1 minute, 1 second to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsMinuteSecond()
         {
             DateTime future = DateTime.Now.AddMinutes(1).AddSeconds(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1m 1s", result);
+            Assert.Equal("1m 1s", result);
         }
 
         /// <summary>
-        /// Test 1h 1m 1s is returned when there is 1 hour, 1 minute, 1 second to go.
+        /// Fact 1h 1m 1s is returned when there is 1 hour, 1 minute, 1 second to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsHourMinuteSecond()
         {
             DateTime future = DateTime.Now.AddHours(1).AddMinutes(1).AddSeconds(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1h 1m 1s", result);
+            Assert.Equal("1h 1m 1s", result);
         }
 
         /// <summary>
-        /// Test 1d 1h 1m 1s is returned when there is 1 day, 1 hour, 1 minute, 1 second to go.
+        /// Fact 1d 1h 1m 1s is returned when there is 1 day, 1 hour, 1 minute, 1 second to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsDayHourMinuteSecond()
         {
             DateTime future = DateTime.Now.AddDays(1).AddHours(1).AddMinutes(1).AddSeconds(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1d 1h 1m 1s", result);
+            Assert.Equal("1d 1h 1m 1s", result);
         }
 
         /// <summary>
-        /// Test 1d 1m 1s is returned when there is 1 hour, 1 minute, 1 second to go.
+        /// Fact 1d 1m 1s is returned when there is 1 hour, 1 minute, 1 second to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsDayMinuteSecond()
         {
             DateTime future = DateTime.Now.AddDays(1).AddMinutes(1).AddSeconds(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1d 1m 1s", result);
+            Assert.Equal("1d 1m 1s", result);
         }
 
         /// <summary>
-        /// Test 1d 1m is returned when there is 1 hour, 1 minute to go.
+        /// Fact 1d 1m is returned when there is 1 hour, 1 minute to go.
         /// </summary>
-        [Test]
+        [Fact]
         public static void ToRemainingTimeShortDescriptionReturnsDayMinute()
         {
             DateTime future = DateTime.Now.AddDays(1).AddMinutes(1);
             string result = future.ToRemainingTimeShortDescription(DateTimeKind.Local);
-            Assert.AreEqual("1d 1m ", result);
+            Assert.Equal("1d 1m ", result);
         }
 
         #endregion
