b90b5a173bf94e0f308936fd5ac7bbeea624372a
Jimi C <jimikar@gmail.com>
2015-07-28 15:35:54 +0300

    Added eve-marketdata as item pricer.
    Former-commit-id: a4e21b37ddc13b1b36fdc8bf460943002cf9b2d1

---
+++ EVEMon.Common/Serialization/EveMarketData/MarketPricer/SerializableEMDItemPriceList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+namespace EVEMon.Common.Serialization.EveMarketData.MarketPricer
+{
+    public sealed class SerializableEMDItemPriceList
+    {
+        private readonly Collection<SerializableEMDItemPriceListItem> m_itemPrices;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerializableEMDItemPriceList"/> class.
+        /// </summary>
+        public SerializableEMDItemPriceList()
+        {
+            m_itemPrices = new Collection<SerializableEMDItemPriceListItem>();
+        }
+
+        [XmlArray("rowset")]
+        [XmlArrayItem("row")]
+        public Collection<SerializableEMDItemPriceListItem> ItemPrices
+        {
+            get { return m_itemPrices; }
+        }
+    }
+}
