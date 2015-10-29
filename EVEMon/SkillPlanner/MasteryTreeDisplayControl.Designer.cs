650291fbc6005f38f4fffdcccda0f865fd38fa35
JimiC <jimikar@gmail.com>
2015-10-29 12:04:17 +0200

    Updating mastery level images.
    Former-commit-id: 06feb37e182d7c8403e85a7a62675ee6cd35c797

--- EVEMon/SkillPlanner/MasteryTreeDisplayControl.Designer.cs
+++ EVEMon/SkillPlanner/MasteryTreeDisplayControl.Designer.cs
@@ -174,12 +174,12 @@
             // 
             this.imageListMasteryLevels.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListMasteryLevels.ImageStream")));
             this.imageListMasteryLevels.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageListMasteryLevels.Images.SetKeyName(0, "level0_24.png");
-            this.imageListMasteryLevels.Images.SetKeyName(1, "level1_24.png");
-            this.imageListMasteryLevels.Images.SetKeyName(2, "level2_24.png");
-            this.imageListMasteryLevels.Images.SetKeyName(3, "level3_24.png");
-            this.imageListMasteryLevels.Images.SetKeyName(4, "level4_24.png");
-            this.imageListMasteryLevels.Images.SetKeyName(5, "level5_24.png");
+            this.imageListMasteryLevels.Images.SetKeyName(0, "masteryLevel0");
+            this.imageListMasteryLevels.Images.SetKeyName(1, "masteryLevel1");
+            this.imageListMasteryLevels.Images.SetKeyName(2, "masteryLevel2");
+            this.imageListMasteryLevels.Images.SetKeyName(3, "masteryLevel3");
+            this.imageListMasteryLevels.Images.SetKeyName(4, "masteryLevel4");
+            this.imageListMasteryLevels.Images.SetKeyName(5, "masteryLevel5");
             // 
             // MasteryTreeDisplayControl
             // 
