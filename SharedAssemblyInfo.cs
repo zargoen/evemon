1be9990d55b11370b4e3e0d3e554b345f10b0652
Jimi C <jimikar@gmail.com>
2015-12-23 00:40:24 +0200

    Correcting references of projects and solution.
    Former-commit-id: a17bc5313825c965e4e16bd95c61aecf69bf1f98

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Friend assembly for unit testing
-[assembly: InternalsVisibleTo("Tests.EVEMon")]
+[assembly: InternalsVisibleTo("Tests.EVEMon.Common")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
