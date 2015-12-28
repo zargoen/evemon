82d8de25901e1a207a94e26bc4a67d13918acab2
Jimi C <jimikar@gmail.com>
2015-12-28 12:26:23 +0200

    Updated copyright year.
    Former-commit-id: a2bcafdb31215022cd6da5468a0438a75b9dcaec

--- tests/Tests.EVEMon.Common/Properties/AssemblyInfo.cs
+++ tests/Tests.EVEMon.Common/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Developement Team")]
 [assembly: AssemblyProduct("EVEMon")]
-[assembly: AssemblyCopyright("Copyright © 2010-2015, EVEMon Development Team")]
+[assembly: AssemblyCopyright("Copyright © 2010-2016, EVEMon Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
