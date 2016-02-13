3d25a432e6d904b24f0509a85cf4afd3b644f079
Jimi C <jimikar@gmail.com>
2016-02-13 11:47:31 +0200

    Moved enumeration to EVEMon.Common.
    Former-commit-id: 1c5ecc9aec53628879d1c65cb142d6d266529dab

--- src/EVEMon/SkillPlanner/AttributesOptimizationForm.cs
+++ src/EVEMon/SkillPlanner/AttributesOptimizationForm.cs
@@ -491,30 +491,4 @@ namespace EVEMon.SkillPlanner
 
         #endregion
     }
-
-    /// <summary>
-    /// Remapping strategy.
-    /// </summary>
-    public enum AttributeOptimizationStrategy
-    {
-        /// <summary>
-        /// Stratagy based on remapping points.
-        /// </summary>
-        RemappingPoints,
-
-        /// <summary>
-        /// Strategy based on the first year from a plan.
-        /// </summary>
-        OneYearPlan,
-
-        /// <summary>
-        /// Strategy based on already trained skills.
-        /// </summary>
-        Character,
-
-        /// <summary>
-        /// Used when the user double-click a remapping point to manually edit it.
-        /// </summary>
-        ManualRemappingPointEdition
-    }
 }
