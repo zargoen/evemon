7d87b35dc98743c4957c403f47518ebae2c3274b
Jimi C <jimikar@gmail.com>
2016-03-28 13:39:38 +0300

    Updated AssemblyInfo.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.4543")]
-[assembly: AssemblyInformationalVersion("3.0.0-RC1")]
+[assembly: AssemblyFileVersion("3.0.0.4559")]
+[assembly: AssemblyInformationalVersion("3.0.0-RC2")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
