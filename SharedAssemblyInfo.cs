502a01e31e27ea5034a445b3767a2b8a87de2855
Blitz Bandis <blitzbandis-evemon@gmail.com>
2016-06-05 17:34:04 -0700

    Release version 3.0.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.4631")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.4636")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
--- SharedAssemblyInfo.template.cs
+++ SharedAssemblyInfo.template.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.$REVNUM$")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.$REVNUM$")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
