a4c7f5af453bfcc56d8f21be457c6b3cf7b14e28
Ashilta <adamarkley@wildair.net>
2017-04-17 22:12:47 +0100

    Fixed the ongoing scopes drama

--- EVEAuthGateway/Controllers/StartupController.cs
+++ EVEAuthGateway/Controllers/StartupController.cs
@@ -48,7 +48,7 @@ namespace EVEMon.Gateways.EVEAuthGateway.Controllers
 
 			foreach (string Scope in ScopeList)
 			{
-				Builder.Append($"{Scope} ");
+				Builder.Append($" {Scope}");
 			}
 
 			return Builder.ToString();
--- EVEAuthGateway/Properties/Settings.Designer.cs
+++ EVEAuthGateway/Properties/Settings.Designer.cs
@@ -29,7 +29,7 @@ namespace EVEMon.Gateways.EVEAuthGateway.Properties
 
 		[global::System.Configuration.ApplicationScopedSettingAttribute()]
 		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-		[global::System.Configuration.DefaultSettingValueAttribute("http://localhost:38163")]
+		[global::System.Configuration.DefaultSettingValueAttribute("http://localhost:38164")]
 		public string InternalServerBaseURL
 		{
 			get
@@ -53,8 +53,58 @@ namespace EVEMon.Gateways.EVEAuthGateway.Properties
 		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
 		[global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
 <ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-  <string>characterContactsRead</string>
-  <string>characterContactsWrite</string>
+<string>corporationContactsRead</string>
+<string>publicData</string>
+<string>characterStatsRead</string>
+<string>characterWalletRead</string>
+<string>characterCalendarRead</string>
+<string>characterFactionalWarfareRead</string>
+<string>characterIndustryJobsRead</string>
+<string>characterMarketOrdersRead</string>
+<string>characterNotificationsRead</string>
+<string>characterResearchRead</string>
+<string>characterSkillsRead</string>
+<string>characterAccountRead</string>
+<string>characterContractsRead</string>
+<string>characterBookmarksRead</string>
+<string>characterClonesRead</string>
+<string>characterOpportunitiesRead</string>
+<string>characterLoyaltyPointsRead</string>
+<string>corporationWalletRead</string>
+<string>corporationAssetsRead</string>
+<string>corporationMedalsRead</string>
+<string>corporationFactionalWarfareRead</string>
+<string>corporationIndustryJobsRead</string>
+<string>corporationKillsRead</string>
+<string>corporationMembersRead</string>
+<string>corporationMarketOrdersRead</string>
+<string>corporationShareholdersRead</string>
+<string>corporationContractsRead</string>
+<string>corporationBookmarksRead</string>
+<string>esi-calendar.respond_calendar_events.v1</string>
+<string>esi-calendar.read_calendar_events.v1</string>
+<string>esi-location.read_location.v1</string>
+<string>esi-location.read_ship_type.v1</string>
+<string>esi-mail.read_mail.v1</string>
+<string>esi-skills.read_skills.v1</string>
+<string>esi-skills.read_skillqueue.v1</string>
+<string>esi-wallet.read_character_wallet.v1</string>
+<string>esi-search.search_structures.v1</string>
+<string>esi-clones.read_clones.v1</string>
+<string>esi-characters.read_contacts.v1</string>
+<string>esi-universe.read_structures.v1</string>
+<string>esi-bookmarks.read_character_bookmarks.v1</string>
+<string>esi-killmails.read_killmails.v1</string>
+<string>esi-corporations.read_corporation_membership.v1</string>
+<string>esi-assets.read_assets.v1</string>
+<string>esi-planets.manage_planets.v1</string>
+<string>esi-markets.structure_markets.v1</string>
+<string>esi-corporations.read_structures.v1</string>
+<string>esi-characters.read_loyalty.v1</string>
+<string>esi-characters.read_opportunities.v1</string>
+<string>esi-characters.read_medals.v1</string>
+<string>esi-characters.read_standings.v1</string>
+<string>esi-characters.read_agents_research.v1</string>
 </ArrayOfString>")]
 		public global::System.Collections.Specialized.StringCollection Scopes
 		{
@@ -66,7 +116,7 @@ namespace EVEMon.Gateways.EVEAuthGateway.Properties
 
 		[global::System.Configuration.ApplicationScopedSettingAttribute()]
 		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-		[global::System.Configuration.DefaultSettingValueAttribute("c496b49614054152b2ce02e859714a0e")]
+		[global::System.Configuration.DefaultSettingValueAttribute("6744fb2bede34f02a21a8becb3647f45")]
 		public string ClientID
 		{
 			get
@@ -77,7 +127,7 @@ namespace EVEMon.Gateways.EVEAuthGateway.Properties
 
 		[global::System.Configuration.ApplicationScopedSettingAttribute()]
 		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-		[global::System.Configuration.DefaultSettingValueAttribute("oGSeVESljDfVwF0ZA3rl252biWKkKOFxbmMGqKYZ")]
+		[global::System.Configuration.DefaultSettingValueAttribute("J2vJrNrHxLPHwdQksLVlL8gaCArKgZ6cCnegb5Jp")]
 		public string ClientSecret
 		{
 			get
