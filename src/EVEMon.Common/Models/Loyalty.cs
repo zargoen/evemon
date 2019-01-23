d958666f275423ab9c26a0e99855a5732ecc055a
Edmond Lee <techhexium@gmail.com>
2019-01-22 22:45:22 -0800

    Add models for Loyalty Points

---
+++ src/EVEMon.Common/Models/Loyalty.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using EVEMon.Common.Serialization.Esi;
+
+namespace EVEMon.Common.Models
+{
+    public sealed class Loyalty
+    {
+        #region Fields
+
+        private readonly Character m_character;
+
+        #endregion
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor from the API.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <param name="src"></param>
+        internal Loyalty(Character character, EsiLoyaltyListItem src)
+        {
+            m_character = character;
+
+            LoyaltyPoints = src.LoyaltyPoints;
+            CorpId = src.CorpID;
+        }
+
+        #endregion
+
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the loyalty point value.
+        /// </summary>
+        /// <value>The loyalty point value.</value>
+        public int LoyaltyPoints { get; }
+
+        /// <summary>
+        /// Gets or sets the corp ID.
+        /// </summary>
+        /// <value>The corp ID.</value>
+        public int CorpId { get; }
+
+        #endregion
+
+
+        #region Helper Methods
+
+
+
+        #endregion
+    }
+}
---
+++ src/EVEMon.Common/Serialization/Esi/EsiLoyaltyListItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace EVEMon.Common.Serialization.Esi
+{
+    [DataContract]
+    public sealed class EsiLoyaltyListItem
+    {
+        [DataMember(Name = "corporation_id")]
+        public int CorpID { get; set; }
+
+        [DataMember(Name = "loyalty_points")]
+        public int LoyaltyPoints { get; set; }
+    }
+}
